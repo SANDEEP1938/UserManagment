fc3c259570d149dd807e9d783e54d3eb2890ce94
sandeep kumar sahu <ssantosh1938@gmail.com>
2026-06-01 13:20:58 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -9,7 +9,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 function App() {
-  console.log("lkjhgfdsa");
   return (
     <SafeAreaProvider>
       <ThemeProvider>
